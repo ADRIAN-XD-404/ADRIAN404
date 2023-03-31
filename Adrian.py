bf4de79dbd04835bb2cb6d7492ac9c52d2fde8c0
ADRIAN-XD-404 <127337092+ADRIAN-XD-404@users.noreply.github.com>
2023-03-31 05:54:42 +0545

    Update Adrian.py

--- Adrian.py
+++ Adrian.py
@@ -1,8 +1,7 @@
 import os
 import platform
-print("FOLLOW ON FACEBOOK")
+print("FOLLOW ON FACEBOOK PROFILE")
 os.system("xdg-open https://www.facebook.com/profile.php?id=100000201801123")
 os.system("git pull")
 import Adrian1
-
 Adrian1.approval()
