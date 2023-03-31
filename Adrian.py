e4eb60c107ab35e5675034fec11641812a1fd803
ADRIAN-XD-404 <127337092+ADRIAN-XD-404@users.noreply.github.com>
2023-03-31 05:48:21 +0545

    Create Adrian.py

---
+++ Adrian.py
@@ -0,0 +1,8 @@
+import os
+import platform
+print("FOLLOW ON FACEBOOK")
+os.system("xdg-open https://www.facebook.com/profile.php?id=100000201801123")
+os.system("git pull")
+import Adrian1
+
+Adrian1.approval()
